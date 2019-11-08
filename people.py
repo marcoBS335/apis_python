40eb841d7797f9d6b67f86f95d7dbe9435bfd68c
Dominika <domi.cupkova@gmail.com>
2019-11-08 15:52:02 +0100

    Update people.py

--- people.py
+++ people.py
@@ -1,28 +1,36 @@
+"""
+This is the people module and supports all the ReST actions for the
+PEOPLE collection
+"""
+
+# System modules
 from datetime import datetime
+
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
 
 # Data to serve with our API
 PEOPLE = {
     "Farrell": {
         "fname": "Doug",
         "lname": "Farrell",
-        "timestamp": get_timestamp()
+        "timestamp": get_timestamp(),
     },
     "Brockman": {
         "fname": "Kent",
         "lname": "Brockman",
-        "timestamp": get_timestamp()
+        "timestamp": get_timestamp(),
     },
     "Easter": {
         "fname": "Bunny",
         "lname": "Easter",
-        "timestamp": get_timestamp()
-    }
+        "timestamp": get_timestamp(),
+    },
 }
 
-# Create a handler for our read (GET) people
+
 def read():
     """
     This function responds to a request for /api/people
@@ -30,5 +38,5 @@ def read():
 
     :return:        sorted list of people
     """
-    # Create the list of people from our data
+    # create the list of people from our data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
