9bc55583cedafe845cffb9140e161db6e28bb2cd
marcoBS335 <56341836+marcoBS335@users.noreply.github.com>
2019-11-11 14:37:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import (
     Flask,
     render_template,
@@ -40,6 +41,33 @@ def home():
 def api_all():
     return jsonify(books)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+	
+	
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
