8a27f5755580002280a8f5d03c5fdde88a5b3812
marcoBS335 <56341836+marcoBS335@users.noreply.github.com>
2020-01-16 10:47:18 +0100

    skuskaApis

--- templates/sqltest.py
+++ templates/sqltest.py
@@ -1,13 +1,13 @@
 import pyodbc
-server = 'restinpythonserver.database.windows.net'
-database = 'Mydatabase'
-username = 'restinpythonadmin'
-password = 'DominikaCupkova1'
+server = 'apis-server.database.windows.net'
+database = 'APIS Database'
+username = 'milan4apis'
+password = 'apisMilan99'
 driver= '{ODBC Driver 17 for SQL Server}'
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-cursor.execute("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid")
+cursor.execute("SELECT TOP 1 FROM [dbo].[skuska_kalkulacka] ORDER BY ID DESC")
 row = cursor.fetchone()
 while row:
-    print (str(row[0]) + " " + str(row[1]))
+    print (str(row[1]) + str(row[3]) + str(row[2]))
     row = cursor.fetchone()
